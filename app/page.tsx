8655c4323e8f2c8c390d41e0a50f0f26aa920620
Ivan <ivanitit3535@gmail.com>
2026-05-21 13:09:30 +0300

    fix:  productGroupList

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       </Container>
       <TopBar />
       <Container className="mt-10 pb-14">
-        <div className="flex gap-10">
+        <div className="flex gap-15">
           <div className="w-[480px]">
             <Filters />
           </div>
@@ -73,6 +73,116 @@ export default function Home() {
                 },
               ]}
               categoryId={1}
+            />
+            <ProductsGroupList
+              title="Combos"
+              items={[
+                {
+                  id: 1,
+                  name: "Margarita",
+                  imageUrl: "/assets/images/products/pizza1.avif",
+                  price: 15,
+                  items: [{ price: 15 }],
+                },
+                {
+                  id: 2,
+                  name: "Margarita",
+                  imageUrl: "/assets/images/products/pizza1.avif",
+                  price: 15,
+                  items: [{ price: 15 }],
+                },
+                {
+                  id: 3,
+                  name: "Margarita",
+                  imageUrl: "/assets/images/products/pizza1.avif",
+                  price: 15,
+                  items: [{ price: 15 }],
+                },
+                {
+                  id: 4,
+                  name: "Margarita",
+                  imageUrl: "/assets/images/products/pizza1.avif",
+                  price: 15,
+                  items: [{ price: 15 }],
+                },
+                {
+                  id: 5,
+                  name: "Margarita",
+                  imageUrl: "/assets/images/products/pizza1.avif",
+                  price: 15,
+                  items: [{ price: 15 }],
+                },
+                {
+                  id: 6,
+                  name: "Margarita",
+                  imageUrl: "/assets/images/products/pizza1.avif",
+                  price: 15,
+                  items: [{ price: 15 }],
+                },
+                {
+                  id: 7,
+                  name: "Margarita",
+                  imageUrl: "/assets/images/products/pizza1.avif",
+                  price: 15,
+                  items: [{ price: 15 }],
+                },
+              ]}
+              categoryId={2}
+            />
+            <ProductsGroupList
+              title="Sides"
+              items={[
+                {
+                  id: 1,
+                  name: "Margarita",
+                  imageUrl: "/assets/images/products/pizza1.avif",
+                  price: 15,
+                  items: [{ price: 15 }],
+                },
+                {
+                  id: 2,
+                  name: "Margarita",
+                  imageUrl: "/assets/images/products/pizza1.avif",
+                  price: 15,
+                  items: [{ price: 15 }],
+                },
+                {
+                  id: 3,
+                  name: "Margarita",
+                  imageUrl: "/assets/images/products/pizza1.avif",
+                  price: 15,
+                  items: [{ price: 15 }],
+                },
+                {
+                  id: 4,
+                  name: "Margarita",
+                  imageUrl: "/assets/images/products/pizza1.avif",
+                  price: 15,
+                  items: [{ price: 15 }],
+                },
+                {
+                  id: 5,
+                  name: "Margarita",
+                  imageUrl: "/assets/images/products/pizza1.avif",
+                  price: 15,
+                  items: [{ price: 15 }],
+                },
+                {
+                  id: 6,
+                  name: "Margarita",
+                  imageUrl: "/assets/images/products/pizza1.avif",
+                  price: 15,
+                  items: [{ price: 15 }],
+                },
+                {
+                  id: 7,
+                  name: "Margarita",
+                  imageUrl: "/assets/images/products/pizza1.avif",
+                  price: 15,
+                  items: [{ price: 15 }],
+                },
+              ]}
+              categoryId={3}
             />
           </div>
         </div>
